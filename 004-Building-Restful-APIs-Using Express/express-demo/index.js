bc9a3f9f894b50709ebbf8aac419cc2b0875227c
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-01 13:08:56 +0530

    Section 04 ( Building Restful APIs uisng Express ) - Handling HTTP POST Requests

--- 004-Building-Restful-APIs-Using Express/express-demo/index.js
+++ 004-Building-Restful-APIs-Using Express/express-demo/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "Course 1" },
   { id: 2, name: "Course 2" },
@@ -21,6 +23,15 @@ app.get("/api/courses/:id", (req, res) => {
   if (!course) {
     res.status(404).send("The course with the given ID was not found.");
   }
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
   res.send(course);
 });
 
