793b4d70fcd9f8fa613e14df194851a1d4331c17
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-01 13:21:25 +0530

    Section 04 ( Building Restful APIs uisng Express ) - Input Validation

--- 004-Building-Restful-APIs-Using Express/express-demo/index.js
+++ 004-Building-Restful-APIs-Using Express/express-demo/index.js
@@ -21,12 +21,18 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id == parseInt(req.params.id));
   if (!course) {
-    res.status(404).send("The course with the given ID was not found.");
+    return res.status(404).send("The course with the given ID was not found.");
   }
   res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
+  if (req.body.name || req.body.name.length < 3) {
+    return res
+      .status(400)
+      .send("Name is required and should be at least 3 characters long.");
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,
