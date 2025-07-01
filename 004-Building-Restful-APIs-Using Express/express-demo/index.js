61b4c20f5663627acbeaa832b8b8665c3718c7eb
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-01 13:02:14 +0530

    Section 04 ( Building Restful APIs uisng Express ) - Handling HTTP GET Requests

--- 004-Building-Restful-APIs-Using Express/express-demo/index.js
+++ 004-Building-Restful-APIs-Using Express/express-demo/index.js
@@ -2,26 +2,26 @@ const express = require("express");
 
 const app = express();
 
+const courses = [
+  { id: 1, name: "Course 1" },
+  { id: 2, name: "Course 2" },
+  { id: 3, name: "Course 3" },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello Client!");
+  res.send("Hello Client");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3, 4, 5]);
+  res.send(courses);
 });
 
-// Route Parameters
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params.id);
-});
-
-app.get("/api/courses/:year/:month", (req, res) => {
-  res.send(req.params);
-});
-
-// Query Parameters
-app.get("/api/courses", (req, res) => {
-  res.send(req.query);
+  const course = courses.find((c) => c.id == parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course with the given ID was not found.");
+  }
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
