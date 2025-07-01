1b47deee36aa74f2ad4ace028e464b282a4158ba
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-01 13:49:55 +0530

    Section 04 ( Building Restful APIs uisng Express ) - Handling HTTP PUT Requests

--- 004-Building-Restful-APIs-Using Express/express-demo/index.js
+++ 004-Building-Restful-APIs-Using Express/express-demo/index.js
@@ -27,13 +27,9 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  const result = schema.validate(req.body);
-  if (result.error) {
-    return res.status(400).send(result.error.details[0].message); // or send(result.error)
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message); // or send(result.error)
   }
 
   const course = {
@@ -43,6 +39,29 @@ app.post("/api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id == parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send("The course with the given ID was not found.");
+  }
+
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message); // or send(result.error)
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
 
 const port = process.env.PORT || 3000;
 
