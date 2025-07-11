23a50d45691f52b53e9d4e7ded8bfb843965f459
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-11 22:50:49 +0530

    Excersise - Added the template engine

--- Excersise-Vidly/Vidly/index.js
+++ Excersise-Vidly/Vidly/index.js
@@ -3,14 +3,24 @@ const genres = require("./routes/genres");
 const express = require("express");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 app.use(express.json());
-app.use("/api/genres", genres);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Vidly App");
+  res.render("index", {
+    title: "Vidly App",
+    heading: "Vidly App",
+    message: "Welcome to the vidly application",
+  });
 });
 
+app.use("/api/genres", genres);
+
 const port = process.env.PORT || 3003;
-router.listen(port, () => {
+
+app.listen(port, () => {
   console.log(`Server is listening on ${port}...`);
 });
