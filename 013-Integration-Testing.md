a979f5b82ca6f5e3e2ca0e8ccdf64512134df92e
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-21 10:56:35 +0530

    Section 13 ( Integration Testing ) - Populating the Test DB

--- 013-Integration-Testing.md
+++ 013-Integration-Testing.md
@@ -15,3 +15,13 @@ To run integration tests, we use a library called **Supertest**.
 For each test, we need to start the server before the test runs and close the server after the test finishes. To do this, we use `beforeEach` to start the server and `afterEach` to close it.
 
 This way, the server is fresh for every test and properly shut down afterward.
+
+# Populating the DB
+
+Each test should run in complete isolation — as if it’s the only test in the world. This means:
+
+- When a test starts, the database should be **clean (empty)**.
+- If the test needs data, it should **insert it at the beginning** of that test.
+- After the test finishes, it should **clean up any data it created**.
+
+This ensures tests are repeatable, reliable, and do not interfere with each other.
--- 013-Integration-Testing/vidly-integration-test/tests/integration/genres.test.js
+++ 013-Integration-Testing/vidly-integration-test/tests/integration/genres.test.js
@@ -1,18 +1,30 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genre");
+
 let server;
 
 describe("/api/genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.deleteMany({});
   });
 
   describe("GET / ", () => {
     it("should return all genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+        { name: "genre3" },
+      ]);
       const res = await request(server).get("/api/genres");
+
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(3);
+      expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre3")).toBeTruthy();
     });
   });
 });
