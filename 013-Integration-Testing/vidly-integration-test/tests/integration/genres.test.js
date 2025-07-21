79464324fb59d51870c95010c980ab75627b4c07
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-21 11:10:48 +0530

    Section 13 ( Integration Testing ) - Testing Routes with Parameters

--- 013-Integration-Testing/vidly-integration-test/tests/integration/genres.test.js
+++ 013-Integration-Testing/vidly-integration-test/tests/integration/genres.test.js
@@ -27,4 +27,21 @@ describe("/api/genres", () => {
       expect(res.body.some((g) => g.name === "genre3")).toBeTruthy();
     });
   });
+
+  describe("GET /:id", () => {
+    it("should return a genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "genre1" });
+      await genre.save();
+
+      const res = await request(server).get("/api/genres/" + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toMatchObject({
+        name: genre.name,
+        _id: genre._id.toString(),
+      });
+      // Alternative
+      // expect(res.body).toHaveProperty("name", genre.name);
+    });
+  });
 });
