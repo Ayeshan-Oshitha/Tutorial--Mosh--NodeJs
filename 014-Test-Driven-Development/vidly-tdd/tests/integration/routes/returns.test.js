9bb59141a9c3ffb6243d3cc136c563017395cf70
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-07-22 18:22:57 +0530

    Section 14 ( Test Driven Development ) - Testing if rental already processed

--- 014-Test-Driven-Development/vidly-tdd/tests/integration/routes/returns.test.js
+++ 014-Test-Driven-Development/vidly-tdd/tests/integration/routes/returns.test.js
@@ -80,4 +80,14 @@ describe("/api/returns", () => {
 
     expect(res.status).toBe(404);
   });
+
+  it("should return 400 if no rental return is already processed", async () => {
+    rental.dateReturned = new Date();
+
+    await rental.save();
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
 });
